ff76cc2e7a80860adc3b01cfec2ac2c8cf29b57a
Parasitedevbio <arthur.talman@ird.fr>
2026-06-10 11:04:34 +0200

    update to bambu devel_pre_v4

--- SCRIPTS/bambu_v2.R
+++ SCRIPTS/bambu_v2.R
@@ -5,14 +5,14 @@ bam_path<-args[1]
 yaml_path<-args[2]
 Csel_name<- args[3]
 
-##### Attention!!!!!!!!!!!!!!!!!! Install bambu via: https://github.com/GoekeLab/bambu/tree/Multiplex_Major_Patch , other version do not work!!!
+##### Attention!!!!!!!!!!!!!!!!!! Install bambu via: https://github.com/GoekeLab/bambu/tree/devel_pre_v4 , other version do not work!!!
 suppressPackageStartupMessages({library(bambu, quietly = T)
-library(NanoporeRNASeq, quietly = T)
-library(yaml, quietly = T)
-library(GGally, quietly = T)
-library(tidyverse, quietly = T)
-library(viridis)})
-  
+  library(NanoporeRNASeq, quietly = T)
+  library(yaml, quietly = T)
+  library(GGally, quietly = T)
+  library(tidyverse, quietly = T)
+  library(viridis)})
+
 ym<-read_yaml(yaml_path)
 
 sample<- ym$sampleinfo$sample_name
@@ -28,31 +28,25 @@ annotation <- prepareAnnotations(gtf_path)
 
 dir.create(paste0(outdir, "/Bambu_",Csel_name ))
 
-print(".....Creating read class file.")
-readClassFile <- bambu(reads = bam_path, annotations = annotation, genome = fa_path, 
-                       ncore = ncores, discovery = FALSE, quant = FALSE, 
-                       demultiplexed = TRUE, verbose = FALSE, assignDist = FALSE, 
-                       yieldSize = 10000000, 
-                       sampleNames = sample, cleanReads = TRUE,dedupUMI = TRUE, stranded = TRUE, trackReads = T)
+sc.opt<-list(extractBarcodeUMI=T,
+             dedupUMI=T,
+             clusters=NULL)
 
-saveRDS(readClassFile, paste0(outdir, "/Bambu_", Csel_name, "/", sample, "_readClassFile_", Csel_name, ".rds"))
 
-print(".....Creating extended annotation.")
-extendedAnno <- bambu(reads = readClassFile, annotations = annotation, genome = fa_path, 
-                      ncore = ncores, discovery = TRUE, quant = FALSE, demultiplexed = TRUE, 
-                      verbose = FALSE, assignDist = FALSE, stranded = TRUE, trackReads = T)
-saveRDS(extendedAnno, paste0(outdir,  "/Bambu_",Csel_name, "/", sample, "_extendedAnno_", Csel_name, ".rds"))
+se<-bambu(reads = bam_path, annotations = annotation, genome = fa_path, 
+                ncore = ncores, discovery = TRUE, quant = TRUE, 
+                verbose = FALSE, assignDist = TRUE, 
+                yieldSize = 10000000, 
+                stranded = TRUE, trackReads = T,
+                opt.singlecell = sc.opt)
 
-print("....Creating quant data")
-quantData <- bambu(reads = readClassFile, annotations = extendedAnno, genome = fa_path, ncore = ncores, discovery = FALSE, quant = FALSE, demultiplexed = TRUE, verbose = FALSE, 
-                   opt.em = list(degradationBias = FALSE), assignDist = TRUE, stranded = TRUE, trackReads = T)
-saveRDS(quantData, paste0(outdir, "/Bambu_",Csel_name, "/", sample,  "_quantData_",Csel_name,".rds"))
+saveRDS(se, paste0(outdir, "/Bambu_", Csel_name, "/", sample,"_",ym$reference$genome_name, "_Bambu_", Csel_name, ".rds"))
 
 print(".....Creating count matrices")
-quantData.gene <- transcriptToGeneExpression(quantData[[1]])
+quantData.gene <- transcriptToGeneExpression(se)
 counts <- assays(quantData.gene)$counts #selecting first sample
 
-saveRDS(counts, paste0(outdir, "/Bambu_",Csel_name, "/", sample, "_counts_", Csel_name,".rds"))
+saveRDS(counts, paste0(outdir, "/Bambu_",Csel_name, "/", sample,"_",ym$reference$genome_name, "_counts_", Csel_name,".rds"))
 
 # Create gene stats
 #### split count matrix from bambu in different ones
@@ -71,7 +65,7 @@ split_counts_genes<- function(counts, patterns, names){
 counts_list<-split_counts_genes(counts = counts, patterns =gene_keys, names = species_names )
 
 for (i in 1:length(counts_list)){
-  saveRDS(counts_list[[i]], paste0(outdir,  "/Bambu_",Csel_name, "/", sample,"_counts_", Csel_name,"_",names(counts_list)[i],".rds" ))
+  saveRDS(counts_list[[i]], paste0(outdir,  "/Bambu_",Csel_name, "/", sample,"_",ym$reference$genome_name,"_counts_", Csel_name,"_",names(counts_list)[i],".rds" ))
 }
 
 #### Create QC
@@ -113,14 +107,14 @@ b<-ggplot()+
 qc<-cowplot::plot_grid(a,b, ncol=1, rel_heights = c(1,1))
 
 ggsave(plot=qc, 
-       filename = paste0(outdir,  "/Bambu_",Csel_name, "/", sample,"_Genes_UMIs_", Csel_name,".pdf" ),
+       filename = paste0(outdir,  "/Bambu_",Csel_name, "/", sample,"_",ym$reference$genome_name,"_Genes_UMIs_", Csel_name,".pdf" ),
        device = "pdf", 
        width = length(counts_list)*50, 
        height = 152,
        units= "mm"
 )
 ggsave(plot=qc, 
-       filename = paste0(outdir,"/Bambu_",Csel_name, "/", sample,"_Genes_UMIs_", Csel_name,".jpg" ),
+       filename = paste0(outdir,"/Bambu_",Csel_name, "/", sample,"_",ym$reference$genome_name,"_Genes_UMIs_", Csel_name,".jpg" ),
        device = "jpeg", 
        width = length(counts_list)*50, 
        height = 152,
@@ -131,33 +125,33 @@ ggsave(plot=qc,
 ### Correlation plots
 qc_df_gene_wide<- qc_df[, c("BC","Species", "Genes")] %>% pivot_wider(names_from = Species, values_from = Genes) %>% column_to_rownames(var = "BC")
 qc_df_UMI_wide<- qc_df[, c("BC","Species", "UMIs")] %>% pivot_wider(names_from = Species, values_from = UMIs) %>% column_to_rownames(var = "BC")
-pm_genes <- ggpairs(qc_df_gene_wide, title = "nGenes")+theme_bw()
-pm_UMIs <- ggpairs(qc_df_UMI_wide, title = "nUMIs")+theme_bw()
+pm_genes <- ggpairs(qc_df_gene_wide)+theme_bw()+ggtitle("nGenes")
+pm_UMIs <- ggpairs(qc_df_UMI_wide)+theme_bw()+ggtitle("nUMIs")
 
 
 ggsave(plot=pm_genes, 
-       filename = paste0(outdir, "/Bambu_",Csel_name, "/", sample,"_Genes_Corr_", Csel_name,".pdf" ),
+       filename = paste0(outdir, "/Bambu_",Csel_name, "/", sample,"_",ym$reference$genome_name,"_Genes_Corr_", Csel_name,".pdf" ),
        device = "pdf", 
        width = length(counts_list)*50, 
        height = (length(counts_list)*50)+10,
        units= "mm"
 )
 ggsave(plot=pm_genes, 
-       filename = paste0(outdir, "/Bambu_",Csel_name, "/", sample,"_Genes_Corr_", Csel_name,".jpg" ),
+       filename = paste0(outdir, "/Bambu_",Csel_name, "/", sample,"_",ym$reference$genome_name,"_Genes_Corr_", Csel_name,".jpg" ),
        device = "jpeg", 
        width = length(counts_list)*50, 
        height = (length(counts_list)*50)+10,
        units= "mm"
 )
 ggsave(plot=pm_UMIs, 
-       filename = paste0(outdir, "/Bambu_",Csel_name, "/", sample,"_UMIs_Corr_", Csel_name,".pdf" ),
+       filename = paste0(outdir, "/Bambu_",Csel_name, "/", sample,"_",ym$reference$genome_name,"_UMIs_Corr_", Csel_name,".pdf" ),
        device = "pdf", 
        width = length(counts_list)*50, 
        height = (length(counts_list)*50)+10,
        units= "mm"
 )
 ggsave(plot=pm_UMIs, 
-       filename = paste0(outdir, "/Bambu_",Csel_name, "/", sample,"_UMIs_Corr_", Csel_name,".jpg" ),
+       filename = paste0(outdir, "/Bambu_",Csel_name, "/", sample,"_",ym$reference$genome_name,"_UMIs_Corr_", Csel_name,".jpg" ),
        device = "jpeg", 
        width = length(counts_list)*50, 
        height = (length(counts_list)*50)+10,
@@ -165,5 +159,5 @@ ggsave(plot=pm_UMIs,
 )
 
 #### Clean
-rm(readClassFile,extendedAnno,quantData,quantData.gene, counts)
+rm(se, counts)
 gc()
